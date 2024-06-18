af5c32d35e9364fc7fc72ff09c6002788b25be62
nabiilawidya <nabiilawidya@7702@gmail.com>
2024-06-18 14:50:02 +0700

    add connection

--- app/src/main/java/com/capstonebangkit/birdwatch/view/ui/analyze/AnalyzeFragment.kt
+++ app/src/main/java/com/capstonebangkit/birdwatch/view/ui/analyze/AnalyzeFragment.kt
@@ -1,5 +1,6 @@
 package com.capstonebangkit.birdwatch.view.ui.analyze
 
+import AnalyzeViewModel
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -10,25 +11,20 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
+import androidx.fragment.app.viewModels
 import com.capstonebangkit.birdwatch.R
-import com.capstonebangkit.birdwatch.data.remote.retrofit.ApiConfig
 import com.capstonebangkit.birdwatch.databinding.FragmentAnalyzeBinding
 import com.capstonebangkit.birdwatch.helper.getImageUri
 import com.capstonebangkit.birdwatch.helper.uriToFile
 import com.capstonebangkit.birdwatch.view.detail.DetailActivity
-import com.capstonebangkit.birdwatch.view.ui.detail.BirdDetailFragment
-import kotlinx.coroutines.launch
-import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.MultipartBody
-import okhttp3.RequestBody.Companion.asRequestBody
 
 class AnalyzeFragment : Fragment() {
 
     private var _binding: FragmentAnalyzeBinding? = null
-    private var currentImageUri: Uri? = null
-
     private val binding get() = _binding!!
+
+    private val analyzeViewModel: AnalyzeViewModel by viewModels()
+    private var currentImageUri: Uri? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -42,7 +38,22 @@ class AnalyzeFragment : Fragment() {
             intentButton.setOnClickListener { startGallery() }
             analyzeButton.setOnClickListener { uploadImage() }
         }
+        observePrediction()
         return root
+    }
+
+    private fun observePrediction() {
+        analyzeViewModel.predictResponse.observe(viewLifecycleOwner){predictResponse ->
+            predictResponse?.let {
+                val intent = Intent(requireContext(), DetailActivity::class.java).apply {
+                    putExtra(DetailActivity.EXTRA_DETAIL, predictResponse)
+                }
+                startActivity(intent)
+            }
+        }
+        analyzeViewModel.loading.observe(viewLifecycleOwner) {
+            showLoading(it)
+        }
     }
 
     private fun startGallery() {
@@ -76,38 +87,7 @@ class AnalyzeFragment : Fragment() {
     private fun uploadImage() {
         currentImageUri?.let { uri ->
             val imageFile = uriToFile(uri, requireContext())
-            Log.d("Image Classification File", "showImage: ${imageFile.path}")
-            showLoading(true)
-
-            val requestImageFile = imageFile.asRequestBody("image/jpeg".toMediaType())
-            val multipartBody = MultipartBody.Part.createFormData(
-                "image",
-                imageFile.name,
-                requestImageFile
-            )
-
-            viewLifecycleOwner.lifecycleScope.launch {
-                try {
-                    val apiService = ApiConfig.getApiService()
-                    val response = apiService.uploadImage(multipartBody)
-                    if (response.isSuccessful) {
-                        val predictResponse = response.body()
-                        Log.d("Response", "Genus: ${predictResponse?.genus}, JenisBurung: ${predictResponse?.jenisBurung}")
-
-                        val intent = Intent(requireContext(), DetailActivity::class.java).apply {
-                            putExtra(DetailActivity.EXTRA_DETAIL, predictResponse)
-                        }
-                        startActivity(intent)
-
-                    } else {
-                        Log.e("UploadImage", "Gagal mengunggah gambar: ${response.message()}")
-                    }
-                } catch (e: Exception) {
-                    Log.e("UploadImage", "Exception: ${e.message}")
-                } finally {
-                    showLoading(false)
-                }
-            }
+            analyzeViewModel.uploadImage(imageFile)
         } ?: showToast(getString(R.string.empty_image_warning))
     }
 
--- app/src/main/java/com/capstonebangkit/birdwatch/view/ui/analyze/AnalyzeViewModel.kt
+++ app/src/main/java/com/capstonebangkit/birdwatch/view/ui/analyze/AnalyzeViewModel.kt
@@ -1,13 +1,50 @@
-package com.capstonebangkit.birdwatch.view.ui.analyze
-
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.capstonebangkit.birdwatch.data.remote.response.PredictResponse
+import com.capstonebangkit.birdwatch.data.remote.retrofit.ApiConfig
+import kotlinx.coroutines.launch
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import java.io.File
 
 class AnalyzeViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+    private val _loading = MutableLiveData<Boolean>()
+    val loading: LiveData<Boolean>
+        get() = _loading
+
+    private val _predictResponse = MutableLiveData<PredictResponse>()
+    val predictResponse: LiveData<PredictResponse>
+        get() = _predictResponse
+
+    fun uploadImage(imageFile: File) {
+        _loading.value = true
+
+        val requestImageFile = imageFile.asRequestBody("image/jpeg".toMediaType())
+        val multipartBody = MultipartBody.Part.createFormData(
+            "image",
+            imageFile.name,
+            requestImageFile
+        )
+
+        viewModelScope.launch {
+            try {
+                val apiService = ApiConfig.getApiService()
+                val response = apiService.uploadImage(multipartBody)
+                if (response.isSuccessful) {
+                    _predictResponse.value = response.body()
+                } else {
+                    Log.e("UploadImage", "Gagal mengunggah gambar: ${response.message()}")
+                }
+            } catch (e: Exception) {
+                Log.e("UploadImage", "Exception: ${e.message}")
+            } finally {
+                _loading.value = false
+            }
+        }
     }
-    val text: LiveData<String> = _text
 }
